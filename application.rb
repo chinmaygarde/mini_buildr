a896c45c276cc42ea4b56b11a862dbe3eb51c9f9
Chinmay Garde <chinmaygarde@gmail.com>
2010-11-11 13:18:47 -0500

    Requires and configuration moved outside application module

--- application.rb
+++ application.rb
@@ -9,22 +9,21 @@ Bundler.require
 require 'resque/server'
 
 module Application
-  # Globals
   ROOT = File.dirname(__FILE__)
   ENVIRONMENT = ENV['environment'] || 'development'
+end
 
-  # Requires
-  %w[helpers controllers jobs models].each do |dir|
-    Dir[File.join(File.dirname(__FILE__), "app", dir, "**/*.rb")].each do |file|
-      require File.expand_path(file)
-    end
+# Requires
+%w[helpers controllers jobs models].each do |dir|
+  Dir[File.join(File.dirname(__FILE__), "app", dir, "**/*.rb")].each do |file|
+    require File.expand_path(file)
   end
-  
-  # Database
-  db = YAML.load_file("db/database.yml")[Application::ENVIRONMENT]
-  Mongoid.configure do |config|
-    name = db["database"]
-    host = db["host"]
-    config.master = Mongo::Connection.new.db(name)
-  end
+end
+
+# Database
+db = YAML.load_file("db/database.yml")[Application::ENVIRONMENT]
+Mongoid.configure do |config|
+  name = db["database"]
+  host = db["host"]
+  config.master = Mongo::Connection.new.db(name)
 end
